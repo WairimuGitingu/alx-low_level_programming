e99eb1f00732b7d9ff20b3d74bd4467ab117a365
WairimuGitingu <wairimugitungu@gmail.com>
2022-06-02 12:26:41 -0700

    get size of types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,2 +1,18 @@
 #include <stdio.h>
 
+/**
+ * main - initialize entry point
+ *description - print out size of data types
+ *types: char, int, long int, long long, float
+ *Return:0
+ */
+
+int main(void)
+{
+	printf("size of char is: %zu bytes.\n", sizeof(char));
+	printf("size of int is: %zu bytes.\n", sizeof(int));
+	printf("size of long int: %zu bytes.\n", sizeof(long int));
+	printf("size of long long: %zu bytes.\n", sizeof(long long));
+	printf("size of float is: %zu bytes.\n", sizeof(float));
+	return (0);
+}
