b2336cdf9193e35d99daea0b1f7d7b1035f45490
WairimuGitingu <wairimugitungu@gmail.com>
2022-06-02 10:48:15 -0700

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
  * description- puts prints out the required ouput
  * return- 0
  */
-int main(void)
+int	main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
