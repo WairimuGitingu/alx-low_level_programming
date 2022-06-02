a6fd5d46a9aaceff05a34761c2a4e938e4fca518
WairimuGitingu <wairimugitungu@gmail.com>
2022-06-02 13:37:11 -0700

    using want to print out

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,7 @@
 
 /**
  * main - intializes 
- * description: display text
+ * description:display text
  *
  * Return:0
  */
@@ -12,4 +12,4 @@ int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
-}	
+}
