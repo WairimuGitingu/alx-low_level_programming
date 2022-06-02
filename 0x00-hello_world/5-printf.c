5108cba86976e367902e5c702499d719e96f4a9a
WairimuGitingu <wairimugitungu@gmail.com>
2022-06-02 11:18:48 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,10 +4,10 @@
  * main - initializes
  * void: nothing initialized
  * description: output the required statement
- * return: returns a value of 0
+ * return: returns the value 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the oucome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
