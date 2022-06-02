b5fdca04a5c53054f4d8c640e240f44e3a80413d
WairimuGitingu <wairimugitungu@gmail.com>
2022-06-02 11:34:31 -0700

    printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 /**
  * main - is the entry point
  * @void - void
- (*
- *description- puts prints out the required ouput)?
+ *
+ *description- puts prints out the required ouput
  * return: returns a value of 0
  */
-int main(void)
+int main(0)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
