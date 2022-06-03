6f5326950e03da93df9e385aa72658a49e584323
WairimuGitingu <wairimugitungu@gmail.com>
2022-06-03 06:42:01 -0700

    The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,9 +14,9 @@ int main(void)
 	int n, last_digit;
 
 	srand(time(0));
-	n = rand() - RAND_MAX /2;
+	n = rand() - RAND_MAX / 2;
 
-	last_digit = n %10;
+	last_digit = n % 10;
 
 	if (last_digit > 5)
 	{
