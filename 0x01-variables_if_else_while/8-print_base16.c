f8613e3f3d0878a3538fa7b21a07e424059de8ad
WairimuGitingu <wairimugitungu@gmail.com>
2022-06-03 10:39:23 -0700

    hexadecimals

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - initialize entry point
+ * declaration : print out hexadecimals using putchar
+ *
+ * Result:0
+ */
+
+int main(void)
+{
+	char hexa_deximal = "0123456789abcdef";
+
+	while (hexa_decimal)
+	{
+		putchar(hexa_decimal);
+	}
+	putchar('\n');
+	return (0);
+}
