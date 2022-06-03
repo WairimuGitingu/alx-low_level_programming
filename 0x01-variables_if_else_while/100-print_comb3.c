65792b3265f567c7bac8b15627820a02650b24c7
WairimuGitingu <wairimugitungu@gmail.com>
2022-06-03 12:01:01 -0700

    combination of 2

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,27 +2,27 @@
 
 /**
  * main - initializes entry
- * declaration : print all combinations 
+ * declaration : print all combinations
  *
  * Return:0
  */
 
 int main(void)
 {
-	int a,b;
+	int a, b;
 
 	a = -1;
 	while (++a < 10)
 	{
-		b = a+1;
+		b = a + 1;
 		while (b < 10)
 		{
 			putchar(a + '0');
 			putchar(b + '0');
-			if ( a == 8 && b == 9)
+			if (a == 8 && b == 9)
 				break;
-			putchar(' ');
 			putchar(',');
+			putchar(' ');
 			b++;
 		}
 	}
