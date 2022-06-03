37dc869b5cf567b822409a52be3e42155ae7b225
WairimuGitingu <wairimugitungu@gmail.com>
2022-06-03 14:19:02 -0700

    all two combinations

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - all possible two combinations
+ * declaration : print all two possibles
+ *
+ * Return:0
+ */
+
+int main(void)
+{
+	int a, b;
+
+	a = -1;
+	while (++a < 100)
+	{
+		b = a + 1;
+		while ( b < 100)
+		{
+			putchar((a / 10) + 48);
+			putchar((a % 10) + 48);
+			putchar(' ');
+			putchar((b / 10) + 48);
+			putchar((b % 10) + 48);
+			if (a == 98 && b == 99)
+				break;
+			putchar(',');
+			putchar(' ');
+			b++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
