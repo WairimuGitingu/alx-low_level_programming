6f99d1980ceafaa9ae6891bec8dec2efde302cd8
WairimuGitingu <wairimugitungu@gmail.com>
2022-06-03 04:53:18 -0700

    completing source code

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define PRINT_SIGN(n) (n < 0 ? printf(%d is negative", n) : n == 0 ? \ printf("%d is zero\n", n) : printf("%d is positive", n))
+
+/**
+ * main - initializes entry point
+ * description: uses macros to determine right condition
+ *
+ *Return:0
+ */
+
+int main(void)
+{
+	int n;
+
+	strand(time(0));
+	n = rand() -RAND_MAX / 2;
+	return (0);
+}
