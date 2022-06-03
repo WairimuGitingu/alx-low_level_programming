fb8a3c289c6a85d2358c1a0fc8f3be64a490deda
WairimuGitingu <wairimugitungu@gmail.com>
2022-06-03 08:21:00 -0700

    print out single digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - initializes entry point
+ * declaration : print all single digit numbers
+ *
+ * Result:0
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		printf(num);
+	}
+	printf("\n");
+	return (0);
+}
