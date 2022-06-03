8c2a9c5d9ab332d614114a95dd48ff5c024b34f3
WairimuGitingu <wairimugitungu@gmail.com>
2022-06-03 09:13:08 -0700

    print out single digits

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+/**
+ * main - initialize entry point
+ * declaration : print out digits with putchar
+ *
+ * Result:0
+ */
 
+int main(void)
+{
+	int n;
+
+	n=-1;
+	while ( n < 10)
+	{
+		putchar(n + '0');
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
