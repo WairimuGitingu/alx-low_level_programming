d19f067d23f99bc2c734f36bcba2736eb640a8f5
WairimuGitingu <wairimugitungu@gmail.com>
2022-06-03 13:36:18 -0700

    3 combinations

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - initialize entry point
- * declaration : 3 digit combinations
+ * main - initialize entry
+ * declaration : enter 3 digits
  *
  * Result:0
  */
@@ -10,27 +10,29 @@
 int main(void)
 {
 	int a,b,c;
-
+	
 	a = -1;
-	while (++a < 10)
+	while (++a < 7)
 	{
-		b = a + 1;
-		while (b < 10)
+		b =  a + 1;
+		while (b < 8)
 		{
 			c = b + 1;
-			while (c < 10)
+			while ( c < 9)
 			{
 				putchar(a + '0');
 				putchar(b + '0');
 				putchar(c + '0');
-				if (a == 7 && b == 8 && c == 9)
+				if( a == 7 && b == 8 && c == 9)
 					break;
 				putchar(',');
 				putchar(' ');
-			
 			}
+			c++;
 		}
+		b++;
 	}
 	putchar('\n');
 	return (0);
 }
+	
