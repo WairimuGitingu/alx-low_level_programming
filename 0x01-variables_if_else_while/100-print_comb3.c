c4b19abdc6cbcd058386de97a0a30f033c9d9503
WairimuGitingu <wairimugitungu@gmail.com>
2022-06-03 11:57:48 -0700

    combination of 2

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - initializes entry
+ * declaration : print all combinations 
+ *
+ * Return:0
+ */
+
+int main(void)
+{
+	int a,b;
+
+	a = -1;
+	while (++a < 10)
+	{
+		b = a+1;
+		while (b < 10)
+		{
+			putchar(a + '0');
+			putchar(b + '0');
+			if ( a == 8 && b == 9)
+				break;
+			putchar(' ');
+			putchar(',');
+			b++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
