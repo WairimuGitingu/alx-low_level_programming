8b7b14d2f29d5e2f2f7af34e0104bd0d3d655c8a
WairimuGitingu <wairimugitungu@gmail.com>
2022-06-07 15:21:12 -0700

    add evens of fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * fibonacci_sum - adds values
+ * @n : integer
+ * declaration : add sum of even numbers of fibonacci
+ * 
+ * Return: void
+ */
+
+void fibonacci_sum(int n)
+{
+	int a, b, c, sum;
+
+	a = 1;
+	b = 2;
+	c = 0;
+	sum = 2;
+
+	while (1)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+		if (b > n)
+			break;
+		if (!(b % 2))
+			sum = sum + b;
+	}
+	printf("%d\n", sum);
+}
+/**
+ * main - calls fibanacci
+ *@void : void
+ *declaration : calls function
+ *Return:0
+ */
+
+int main(void)
+{
+	fibonacci_sum(4000000);
+	return (0);
+}
