a4d9206ee62851348985a05f6b424b1bf5485647
WairimuGitingu <wairimugitungu@gmail.com>
2022-06-07 12:36:37 -0700

    add 2 values

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - add two values
+ * @a: integer
+ * @b: integer
+ * declaration : add two variable values
+ *
+ * Return:integer
+ */
+
+int add(int a, int b)
+{
+	int c;
+
+	c = sum(a, b);
+	return (c);
+}
