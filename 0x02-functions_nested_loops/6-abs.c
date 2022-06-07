30327cc7fb65d8833eea48a5d783ca6012a6be4e
WairimuGitingu <wairimugitungu@gmail.com>
2022-06-07 08:32:09 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * abs - computes absolute value of a value
+ * @int - integer
+ *description : computes integer
+ *
+ * Return:int
+ */
+
+int _abs(int)
+{
+	int c;
+
+	if( c > 0)
+	{
+		c = +(c);
+	}
+	else
+	{
+		c = -(c);
+	}
+}
