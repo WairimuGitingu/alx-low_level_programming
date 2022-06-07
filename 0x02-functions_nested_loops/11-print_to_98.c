4e03212f89e53a5c152a904b1199024682d27466
WairimuGitingu <wairimugitungu@gmail.com>
2022-06-07 13:40:47 -0700

    numbers to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,18 +11,24 @@
 
 void print_to_98(int n)
 {
-	if (n <= 98)
+	if (n >= 98)
 	{
-		_putchar(n + '0');
-		_putchar(',');
-		_putchar(' ');
-		n++;
+		for (; n >= 98; n--)
+		{
+			printf("%d", n);
+			if (n == 98)
+				break;
+			printf(", ");
+		}
 	}
 	else
 	{
-		_putchar(n + '0');
-		_putchar(',');
-		_putchar(' ');
-		n--;
+		for (; n <=98; n++)
+		{
+			printf("%d", n);
+			if (n == 98)
+				break;
+			printf(", ");
+		}
 	}
 }
