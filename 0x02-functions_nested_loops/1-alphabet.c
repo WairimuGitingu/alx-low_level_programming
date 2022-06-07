0a192553dd6943c3904683d424bbe31c78fe5320
WairimuGitingu <wairimugitungu@gmail.com>
2022-06-07 03:08:21 -0700

    print out characters

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return : always 0.
+ */
+
+void print_alphabet(void);
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+{
+	int alphabets;
+
+	for (alphabets = 'a', alphabets <= 'z', alphabets++)
+	{
+		putchar(alphabets);
+	}
+	puchar('\n');
+}
