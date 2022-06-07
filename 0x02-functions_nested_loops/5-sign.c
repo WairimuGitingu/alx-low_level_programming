70cf69bf1101563d91530539b68dec247fa7243d
WairimuGitingu <wairimugitungu@gmail.com>
2022-06-07 07:17:40 -0700

    positive, zero or negative

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_sign : return sign
+ * @n: int
+ *description : return sign according to condition
+ *Return:+ if positive, 0 if zero and - if negative
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar ('+');
+		return (0);
+	}
+	else if (n = 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (0);
+	}
+}
