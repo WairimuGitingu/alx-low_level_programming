36d7e5de19ab2691d614cc75b24dbbeca63108b9
WairimuGitingu <wairimugitungu@gmail.com>
2022-06-07 15:04:08 -0700

    fibanacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+/**
+ * fibonacci - print out first 50 number of fibonacci
+ * @n : integer
+ * declaration : print out 50 numbers 
+ *
+ * Return: n
+ */
+
+void fibonacci(int n)
+{
+	unsigned long a, b, c;
+
+	a = 1;
+	b = 2;
+	c = 0;
+
+	printf("1,2, ");
+	n = n - 2;
+	while(n--)
+	{
+		printf("%lu", c = a + b);
+		if (n)
+		{
+			printf(", ");
+		}
+		a = b;
+		b = c;
+	}
+	printf("\n");
+}
+
+/**
+ * main - entry point
+ * @void: void
+ * description : calls the fibonacci function
+ *
+ * Result: int
+ */
+
+int main(void)
+{
+	fibanacci(50);
+	return (0);
+}
