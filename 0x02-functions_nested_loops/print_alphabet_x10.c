3ba4f29450c185637af1d69b8a9169057b4a3969
WairimuGitingu <wairimugitungu@gmail.com>
2022-06-07 04:55:29 -0700

    print out letters x10

---
+++ 0x02-functions_nested_loops/print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function name
+ * declaration : print out alphabets 10 times
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int alphabets = 'a';
+	int b = 0;
+
+	for (int b <= 10)
+	{
+		while (alphabets <= 'z')
+		{
+			_putchar(alphabets);
+			_putchar('\n');
+			alphabet++;
+		}
+		b++;
+	}
+}
