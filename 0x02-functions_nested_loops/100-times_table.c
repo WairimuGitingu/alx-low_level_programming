2555b5bbe1bcdbb97ffff195c5e73e45feb2e92b
WairimuGitingu <wairimugitungu@gmail.com>
2022-06-08 12:46:25 -0700

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -63,18 +63,39 @@ void print_times_table(int n)
 {
 	int a, b, c;
 
-	if (n > -1 && n < 16)
+	if (n >= 0 && n <= 15)
 	{
-		for (a = 0; a < n; a++)
+		for (a = 0; a <= n; a++)
 		{
-			for (b = 0; b < n; b++)
+			for (b = 0; b <= n; b++)
 			{
-				c = a * b;
-				print_num(c);
-				if (b == n)
-					break;
-				_putchar(',');
-				print_spaces(c + a);
+				c = b * a;
+				if (b == 0)
+					_putchar(c + '0');
+				else if (c < 10 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(c + '0');
+				}
+				else if (c >= 10 && c < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((c / 10) + '0');
+					_puchar((c % 10) + '0');
+				}
+				else if (c >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((c / 100) + '0');
+					_putchar(((c / 10) % 10) + '0');
+					_putchar((c % 10) + '0');
+				}
 			}
 			_putchar('\n');
 		}
