ae945768a11e3c55e316d89d4d4af1b18fa6a125
WairimuGitingu <wairimugitungu@gmail.com>
2022-06-08 13:23:09 -0700

    multiple

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,12 +12,12 @@
 void times_table(void)
 {
 	int a, b, c;
+
 	for (a = 0; a < 10; a++)
 	{
 		for (b = 0; b < 10; b++)
 		{
 			c = b * a;
-			
 			if (b == 0)
 				_putchar(c + '0');
 			if (c < 10 && b != 0)
