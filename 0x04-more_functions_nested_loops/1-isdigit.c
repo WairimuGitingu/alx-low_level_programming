692bdde5db2685a83ed9c1717bcc404aec039a7e
WairimuGitingu <wairimugitungu@gmail.com>
2022-06-09 14:13:49 -0700

    checks ifdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -4,13 +4,12 @@
  * _isdigit - checks if digit
  * @c: integer
  *
- * Return: 0 or 1
+ * Return: 1 or 0
  */
 
 int _isdigit(int c)
 {
 	if (c >= 0 && c <= 9)
 		return (1);
-	else
 		return (0);
 }
