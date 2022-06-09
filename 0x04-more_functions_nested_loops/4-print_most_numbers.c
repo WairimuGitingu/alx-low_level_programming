10e23fda0d7022726b8a7a9eed7511c2e0034310
WairimuGitingu <wairimugitungu@gmail.com>
2022-06-09 10:05:29 -0700

    01356789

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_most_number - print digits between 0 and 9 except 2 and 4
+ * @void: void
+ * description: print out all values except 2 and 4
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		while (a != 2 && a != 4)
+		{
+			printf("%d", a);
+		}
+		printf("\n");
+	}
+}
