9f5ad54f9a622c728f3e8b6cce420b55c22dc3d5
WairimuGitingu <wairimugitungu@gmail.com>
2022-06-09 11:56:32 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - prints out line with _
+ * @n: integer
+ * description : print out a line
+ *
+ * Return:0
+ */
+
+void print_line(int n)
+{
+	while (n++)
+	{
+		if (n <= 0)
+			break;
+		putchar('_');
+	}
+	putchar('\n');
+}
