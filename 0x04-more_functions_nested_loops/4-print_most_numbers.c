80ec72b87dbaff9e8c57157b4556b103e17820e9
WairimuGitingu <wairimugitungu@gmail.com>
2022-06-09 17:57:39 -0700

    most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,9 +12,9 @@ void print_most_numbers(void)
 {
 	char c;
 
-	for (c  = 0; c <= 9; c++)
+	for (c  = '0'; c <= '9'; c++)
 	{
-		while (c != 2 && c != 4)
+		while (c != '2' && c != '4')
 		{
 			_putchar(c);
 		}
