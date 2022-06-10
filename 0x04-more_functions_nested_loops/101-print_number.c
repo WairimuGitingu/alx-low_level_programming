f7b3dac44bcbfc8008d6aef93887d10dde532e7b
WairimuGitingu <wairimugitungu@gmail.com>
2022-06-09 18:20:59 -0700

    prints

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,4 +10,32 @@
 
 void print_number(int n)
 {
-	int 
+	int copy, nth, size = 1, ones = n % 10;
+
+	n = n / 10;
+	copy = n;
+
+	if (ones < 0)
+	{
+		ones = ones * -1;
+		copy = copy * -1;
+		n = n * -1;
+		_putchar('-');
+	}
+	if (copy > 0)
+	{
+		while (copy / 10 != 0)
+		{
+			copy = copy / 10;
+			size - size * 10;
+		}
+		while (size > 0)
+		{
+			nth = n / size;
+			_putchar('0' + nth);
+			n -= nth * size;
+			size = size / 10;
+		}
+	}
+	_putchar('0' + ones);
+}
