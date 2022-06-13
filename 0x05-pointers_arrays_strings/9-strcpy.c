faa70d4e861e9740404527f8cf0742df5d5214c8
WairimuGitingu <wairimugitungu@gmail.com>
2022-06-13 15:05:43 -0700

    copies strings

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -13,15 +13,15 @@ char *_strcpy(char *dest, char *src)
 {
 	int l = 0;
 	int z = 0;
-
-	 while (src[l] != '\0')
-	 {
-		 l++;
-	 }
-	 for (; z < l; z++)
-	 {
-		 dest[z] = src[z];
-	 }
-	 dest[l] = '\0';
-	 return (dest);
+	
+	while (src[l] != '\0')
+	{
+		l++;
+	}
+	for (; z < l; z++)
+	{
+		dest[z] = src[z];
+	}
+	dest[l] = '\0';
+	return (dest);
 }
