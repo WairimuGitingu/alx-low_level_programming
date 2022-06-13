aef0bccd0261f8e846af6a7a33ca19bfe5ab1cfc
WairimuGitingu <wairimugitungu@gmail.com>
2022-06-13 10:10:45 -0700

    finds the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @s: char
+ *
+ * Return: integer
+ */
+
+int _strlen(char *s)
+{
+	strlen(s);
+}
