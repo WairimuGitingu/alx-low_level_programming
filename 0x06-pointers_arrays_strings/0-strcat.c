3c3025ff9364181c8a2c7870b79a308783991b75
WairimuGitingu <wairimugitungu@gmail.com>
2022-06-15 11:19:10 -0700

    concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,14 +11,18 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
+	int i = 0, j = 0;
 
-	for (i = 0; dest[i] != '\0'; i++)
-		for (j = 0; src[j] != '\0'; j++)
-		{
-			dest[i] = src[j];
-			i++;
-		}
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
 	dest[i] = '\0';
 	return (dest);
 }
