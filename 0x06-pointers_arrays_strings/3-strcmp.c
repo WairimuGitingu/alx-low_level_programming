d782c9312baf1b4b559e838e1bbcb5977ad35f04
WairimuGitingu <wairimugitungu@gmail.com>
2022-06-16 04:40:41 -0700

    compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,25 +10,15 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, j;
+	int i = 0;
 
-	for (i = 0; s1[i] != '\0'; i++)
+	while(s1[i] != '\0' && s2[i] != '\0')
 	{
-		for (j = 0; s2[j] != '\0'; j++)
+		if (s1[i] != s2[i])
 		{
-			if (i == j)
-			{
-				return (0);
-			}
-			else if (i > j)
-			{
-				return (15);
-			}
-			else
-			{
-				return (-15);
-			}
+			return (s1[i] - s2[i]);
 		}
+		i++;
 	}
 	return (0);
 }
