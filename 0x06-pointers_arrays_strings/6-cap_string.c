5cd0ffdb8fec64f50439781a76c948c98fc2f467
WairimuGitingu <wairimugitungu@gmail.com>
2022-06-16 03:25:03 -0700

    capitalize first letters in a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @n: char
+ *
+ * Return:n
+ */
+
+char *cap_string(char *n)
+{
+	int i;
+
+	if (n[0] >= 'a' && n[0] <= 'z')
+	{
+		n[0] = n[0] - 32;
+	}
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		switch (n[i])
+		{
+			case ' ':
+			case '\t':
+			case '\n':
+			case ';':
+			case ',':
+			case '.':
+			case '!':
+			case '?':
+			case '"':
+			case '(':
+			case ')':
+			case '{':
+			case '}':
+			if (n[i] > 96 && n[i] < 123)
+			{
+				n[i] = n[i] - 32;
+			}
+		}
+	}
+	return (n);
+}
