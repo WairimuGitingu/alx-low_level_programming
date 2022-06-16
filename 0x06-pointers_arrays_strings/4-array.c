5cb4506dfdd21241a64eb84c956fbe202d5f91f6
WairimuGitingu <wairimugitungu@gmail.com>
2022-06-16 01:22:52 -0700

    reverse array

--- 0x06-pointers_arrays_strings/4-array.c
+++ 0x06-pointers_arrays_strings/4-array.c
@@ -10,13 +10,18 @@
 
 void reverse_array(int *a, int n)
 {
-	int i;
+	int i, temp;
 
 	i = 0;
 	while (i < (n / 2))
 	{
+		temp = a[i];
 		a[i] = a[n - i - 1];
-		a[n - i - 1] = a[i];
+		a[n - i - 1] = temp;
+		i++;
+	}
+	while (i < n)
+	{
 		i++;
 	}
 }
