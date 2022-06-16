dbfa554a4c46f6f3b5e486eb8e6c530b09960df4
WairimuGitingu <wairimugitungu@gmail.com>
2022-06-16 14:40:38 -0700

    prints out integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,12 +9,17 @@
 
 void print_number(int n)
 {
+	unsigned int i;
+	
+	i = n;
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		i = -n;
 	}
 	if (n / 10 != 0)
+	{
 		print_number(n / 10);
-	_putchar(n % 10 + '0');
+	}
+	_putchar((n % 10) + '0');
 }
