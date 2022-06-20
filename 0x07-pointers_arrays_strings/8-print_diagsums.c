3324cb6dbf86e3b203f54e3269d1105552dbd73b
WairimuGitingu <wairimugitungu@gmail.com>
2022-06-20 13:14:49 -0700

    add diagonal

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * print_diagsums- adds sum of two diagonals of a square matrix of integers
+ * @a: int
+ * @size: int
+ *
+ * Return:void
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int c = 0;
+	int l = 0;
+	int sq = size * size;
+
+	for (i = 0; i < sq; i = i + (size + 1))
+	{
+		c = c + a[i];
+	}
+	for (i = size - 1; i < sq - 1; i = i + (size - 1))
+	{
+		l = l + a[i];
+	}
+	printf("%d%d\n", c, l);
+}
