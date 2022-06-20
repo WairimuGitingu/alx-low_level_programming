98e549e1abbfef12c32982bb3444b450792a2703
WairimuGitingu <wairimugitungu@gmail.com>
2022-06-20 10:25:25 -0700

    length

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,11 +12,11 @@ unsigned int _strspn(char *s, char *accept)
 {
 	int i;
 	int j;
-	unsigned int length = 0;
+	unsigned int length;
 
+	length = 0;
 	for (i = 0; s[i] != '\0'; i++)
 		for (j = 0; accept[j] != '\0' && accept[j] != s[i]; j++)
-			;
 		{
 			if (s[i] == accept[j])
 				length++;
