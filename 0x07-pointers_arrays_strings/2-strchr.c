4dae7a6fdedf84e6290b124d75cef46e0c76e12b
WairimuGitingu <wairimugitungu@gmail.com>
2022-06-20 10:10:35 -0700

    length

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -18,6 +18,7 @@ char *_strchr(char *s, char c)
 		if (s[i] == c)
 		{
 			return (&s[i]);
+			i++;
 		}
 		i++;
 	}
---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefic substring
+ * @s: string to look into
+ * @accept: string with the characters
+ *
+ * Return:length
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int j;
+	unsigned int length = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				length++;
+			if (accept[j] == '\0')
+				return (length);
+		}
+	return (length);
+}
