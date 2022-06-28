11198cf338d484f24139246be8ad7c1fee555129
WairimuGitingu <wairimugitungu@gmail.com>
2022-06-28 08:09:03 -0700

    concatenates strungs

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,15 +15,19 @@ char *str_concat(char *s1, char *s2)
 	char *t;
 	int i, j;
 
-	i = 0;
-	j = 0;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	i = j = 0;
 	while (s1[i] != '\0')
 		i++;
 	while (s2[j] != '\0')
 		j++;
-	t = malloc(sizeof(char) * i + j + 1);
+	t = malloc(sizeof(char) * (i + j + 1));
 	if (t == NULL)
 		return (NULL);
+	i = j =0;
 	while (s1[i] != '\0')
 	{
 		t[i] = s1[i];
