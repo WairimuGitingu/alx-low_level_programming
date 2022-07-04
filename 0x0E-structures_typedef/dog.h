8430429ca40348a2a9a97251d63e3c200298f847
WairimuGitingu <wairimugitungu@gmail.com>
2022-07-04 09:49:45 -0700

    type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE dog.h
+
+/**
+ * struct dog - new struct with name, age and owner
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
