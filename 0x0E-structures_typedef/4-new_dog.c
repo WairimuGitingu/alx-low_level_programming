06e8305ecfb3413aebe9504415e1dfdc2a42c0de
WairimuGitingu <wairimugitungu@gmail.com>
2022-07-04 13:29:46 -0700

    create new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,10 +3,12 @@
 #include "dog.h"
 
 /**
- * new_dog-  creates a new dog
+ * new_dog - creates a new dog
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
+ *
+ * Return: pointer to a new dog
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
