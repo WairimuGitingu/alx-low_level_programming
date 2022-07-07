91b07312a1a4a83f98b8200a2a6cc7106b794b59
WairimuGitingu <wairimugitungu@gmail.com>
2022-07-07 05:02:51 -0700

    print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,4 +12,17 @@
 
 void print_numbers(const char*separator, const unsigned int i, ...)
 {
+	va_list ap;
+	unsigned int i;
 
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(ap, unsigned int));
+		if (i < (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+	va_end(ap);
+	printf("\n");
+}
