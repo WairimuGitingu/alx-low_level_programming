c61fd0e83bbeb74643cfa7b085133a641c0f7ef6
WairimuGitingu <wairimugitungu@gmail.com>
2022-07-07 04:58:04 -0700

    .

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,15 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - prints all numbers
+ * @separator: separates two values
+ * @n: int to be printed
+ *
+ * Return: void
+ */
+
+void print_numbers(const char*separator, const unsigned int i, ...)
+{
+
