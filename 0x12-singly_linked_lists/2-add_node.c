1f3c952c87088447652d51c4fdd76695e4a2a00b
WairimuGitingu <wairimugitungu@gmail.com>
2022-07-14 12:16:56 -0700

    add node in the beginning

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -18,17 +18,21 @@ list_t *add_node(list_t **head, const char *str)
 	newNode = malloc(sizeof(list_t));
 	if (newNode == NULL)
 		return (NULL);
+
 	temp = strdup(str);
 	if (temp == NULL)
 	{
 		free(newNode);
 		return (NULL);
 	}
+
 	for (len = 0; str[len];)
 		len++;
+
 	newNode->str = temp;
 	newNode->len = len;
 	newNode->next = *head;
 	*head = newNode;
+
 	return (newNode);
 }
