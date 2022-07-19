b724f618e8c85698fb41bcc06ddb3ba4e2ca6699
WairimuGitingu <wairimugitungu@gmail.com>
2022-07-19 06:42:33 -0700

    reverses nodes

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+/**
+ * reverse_listint - reverses the linked list
+ * @head: node to be reversed
+ *
+ * Return: 0
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	head++;
+	return (0);
+}
